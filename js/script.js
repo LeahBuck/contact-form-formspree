48c226324dbc260211497a4c297ac8b4b4e5ff59
mwallert <michael.wallert@hotmail.com>
2017-05-13 11:34:26 -0700

    revert

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 (function(){
   var myForm = document.getElementById('emailForm');
   var submitButton = document.getElementById('submitButton');
-  
+
   myForm.addEventListener('submit', function(event) {
     myForm.setAttribute('action', 'https://formspree.io/micheal.wallert@gmail.com');
   });
