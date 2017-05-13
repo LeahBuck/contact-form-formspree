85b0d161f1b2ab60becc86f399ee50357adf1358
mwallert <michael.wallert@hotmail.com>
2017-05-13 00:42:26 -0700

    Added johndoe email to test with

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
   var submitButton = document.getElementById('submitButton');
   
   myForm.addEventListener('submit', function(event) {
-    myForm.setAttribute('action', 'https://formspreee.io/');
+    myForm.setAttribute('action', 'https://formspreee.io/johndoe919909@gmail.com');
     return true;
   });
 }());
