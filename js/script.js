c5aecf27989f65f676ea996a0f01d3e9c7e85201
mwallert <michael.wallert@hotmail.com>
2017-05-13 00:37:01 -0700

    Email setup

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
 (function(){
-  var myForm = document.getElementById('EmailForm');
-  var submitButton = document.getElementById('SubmitButton');
-  var message = document.getElementById('Message');
-  var charactersDisplay = document.getElementById('MaxCharacters');
-  var charactersRemaining = 150;
-
-  message.addEventListener('focus', function(event) {
-    var charactersUsed = message.value.length;
-    charactersDisplay.textContent = "Characters remaining: " + (charactersRemaining - charactersUsed);
-  });
-
-  message.addEventListener('keydown', function(event) {
-    var charactersUsed = message.value.length;
-    charactersDisplay.textContent = "Characters remaining: " + (charactersRemaining - charactersUsed);
-  });
-
+  var myForm = document.getElementById('emailForm');
+  var submitButton = document.getElementById('submitButton');
+  
   myForm.addEventListener('submit', function(event) {
-    event.preventDefault();
+    myForm.setAttribute('action', 'https://formspreee.io/');
+    return true;
   });
 }());
