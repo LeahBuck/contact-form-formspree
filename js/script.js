5999b0d695aca1920c1eef5a9ae5c572d25319a5
mwallert <michael.wallert@hotmail.com>
2017-05-13 01:05:57 -0700

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 (function(){
+  var $ = jQuery;
   var myForm = document.getElementById('emailForm');
   var submitButton = document.getElementById('submitButton');
   
   myForm.addEventListener('submit', function(event) {
-    myForm.setAttribute('action', 'https://formspreee.io/');
-    return true;
+    // myForm.setAttribute('action', 'https://formspree.io/johndoe919909@gmail.com');
+    $.ajax({
+        url: "https://formspree.io/michael.wallert@gmail.com", 
+        method: "POST",
+        data: {message: "hello!"},
+        dataType: "json"
+    });
   });
 }());
